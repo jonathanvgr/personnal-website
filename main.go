c2c58a92c6146794b4e82f4bd11bcbc63059f6c9
Jonathan <jonathanvgr27@gmail.com>
2022-11-16 11:16:37 +0100

    simple server

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	println("Hello World")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		fmt.Fprintf(c.Writer, "Hello World")
+	})
+
+	r.Run()
 }
