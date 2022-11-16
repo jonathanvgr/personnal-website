35ba6fa00c4ab9a9ad423a40c3a5021c0616f1cf
Jonathan <jonathanvgr27@gmail.com>
2022-11-16 23:48:26 +0100

    todo api

--- backend/apps/todo/controller.go
+++ backend/apps/todo/controller.go
@@ -6,8 +6,8 @@ import (
 )
 
 func GetAll(c *gin.Context) {
-	var todos []Todo
 	db := common.GetDB()
+	var todos []Todo
 
 	if err := db.Find(&todos).Error; err != nil {
 		common.Send_API_Error(c, err)
@@ -18,11 +18,38 @@ func GetAll(c *gin.Context) {
 }
 
 func Get(c *gin.Context) {
+	db := common.GetDB()
 
+	id := c.Param("id") // Get ID from URL
+	var todo Todo
+
+	// Get first entry in DB where (Todo DB ID == ID from URL)
+	if err := db.First(&todo, id).Error; err != nil {
+		common.Send_API_Error(c, err)
+		return
+	}
+
+	common.Send_API_Data(c, todo)
 }
 
 func Create(c *gin.Context) {
+	db := common.GetDB()
 
+	var todo Todo
+
+	// Check if JSON body data binds with Todo struct model
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		common.Send_API_BadRequest(c, err)
+		return
+	}
+
+	// Create entry in DB
+	if err := db.Create(&todo).Error; err != nil {
+		common.Send_API_Error(c, err)
+		return
+	}
+
+	common.Send_API_Data(c, todo) // Return created todo
 }
 
 func Update(c *gin.Context) {
@@ -30,5 +57,14 @@ func Update(c *gin.Context) {
 }
 
 func Delete(c *gin.Context) {
+	db := common.GetDB()
+	id := c.Param("id") // Get ID from URL
 
+	// Permanently (with Unscoped) delete record from DB
+	if err := db.Unscoped().Delete(&Todo{}, id).Error; err != nil {
+		common.Send_API_Error(c, err)
+		return
+	}
+
+	common.Send_API_Data(c, true)
 }
