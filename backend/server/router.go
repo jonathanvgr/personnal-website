f0ac9dcf2e550766362c30969ec199bb42b513cc
Jonathan <jonathanvgr27@gmail.com>
2022-11-18 11:44:42 +0100

    added cors

--- backend/server/router.go
+++ backend/server/router.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/jonathanvgr/website/apps/note"
@@ -8,7 +9,7 @@ import (
 
 func setupRouter() *gin.Engine {
 	router := gin.New()
-
+	router.Use(cors.Default())
 	// Serve static file and redirect to index when no corresponding route is found
 	router.Use(static.Serve("/", static.LocalFile("./frontend", true)))
 	router.NoRoute(func(c *gin.Context) {
