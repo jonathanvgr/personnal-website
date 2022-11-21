83a58aa11155567dc1add82bf2db821212abf3fe
Jonathan <jonathanvgr27@gmail.com>
2022-11-21 17:41:32 +0100

    env changes

--- backend/go.mod
+++ backend/go.mod
@@ -4,9 +4,7 @@ go 1.19
 
 require (
 	github.com/gin-contrib/cors v1.4.0
-	github.com/gin-contrib/static v0.0.1
 	github.com/gin-gonic/gin v1.8.1
-	github.com/joho/godotenv v1.4.0
 	gorm.io/driver/sqlite v1.4.3
 	gorm.io/gorm v1.24.1
 )
--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
 	"github.com/jonathanvgr/website/server"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	server.Run()
 }
