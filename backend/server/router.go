88bb0ed8f473cb3c01d4a7eee82c261caeaf879e
Jonathan <jonathanvgr27@gmail.com>
2022-11-21 17:14:17 +0100

    reverse proxy to next app

--- backend/server/router.go
+++ backend/server/router.go
@@ -1,11 +1,33 @@
 package server
 
 import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
 	"github.com/gin-contrib/cors"
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/jonathanvgr/website/apps/note"
 )
+
+func nextProxy(c *gin.Context) {
+	remote, err := url.Parse("http://localhost:3000/")
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL = c.Request.URL
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+	}
+
+	proxy.ServeHTTP(c.Writer, c.Request)
+}
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
@@ -15,12 +37,7 @@ func setupRouter() *gin.Engine {
 	corsConfig.AllowWildcard = true
 	router.Use(cors.New(corsConfig))
 
-	// Serve static file and redirect to index when no corresponding route is found
-	router.Use(static.Serve("/", static.LocalFile("./frontend", true)))
-	router.NoRoute(func(c *gin.Context) {
-		c.Request.URL.Path = "/"
-		router.HandleContext(c)
-	})
+	router.NoRoute(nextProxy)
 
 	// API routes
 	api := router.Group("api")
